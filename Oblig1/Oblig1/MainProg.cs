e068f02f61c1e1307b6880206072730af8e71620
Storm <sangolts@gmail.com>
2024-02-28 14:37:55 +0100

    Litt av task 3 gjort

---
+++ Oblig1/Oblig1/MainProg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSim;
+
+class Astronomy
+{
+    public static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject>
+        {
+            new Star("Sun", 100, "Yellow"),
+            new Planet("Mercury", 10, 58, "Gray", 57910, 88),
+            new Planet("Venus", 30, 243, "Yellow", 108200, 225),
+            new Planet("Terra", 20, 24, "Blue", 149600, 365),
+            new Moon("The Moon", 4, 27, "Gray", 384, 27),
+            new DwarfPlanet("Pluto", 3, 150, "Brown", 5906000, 9052),
+        };
+
+        foreach (SpaceObject obj in solarSystem)
+        {
+            obj.Draw();
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ Oblig1/Oblig1/SpaceObject.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace SpaceSim
+{
+    public class SpaceObject
+    {
+        protected string name;
+        protected double objectRadius;
+        protected string objectColor;
+
+        public SpaceObject(string name, double objectRadius, string objectColor)
+        {
+            this.name = name;
+            this.objectRadius = objectRadius;
+            this.objectColor = objectColor;
+        }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine(name);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double ObjectRadius
+        {
+            get { return objectRadius; }
+            set { objectRadius = value; }
+        }
+
+        public string ObjectColor
+        {
+            get { return objectColor; }
+            set { objectColor = value; }
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(string name, double objectRadius, string objectColor) : base(name, objectRadius, objectColor)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Star : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        protected double orbitalRadius; // in kilometers
+        protected double orbitalPeriod; // in days
+        protected double rotationalPeriod; // in hours
+
+        public Planet(string name, double objectRadius, double rotationalPeriod, string objectColor,
+            double orbitalRadius, double orbitalPeriod)
+            : base(name, objectRadius, objectColor)
+        {
+            this.orbitalRadius = orbitalRadius;
+            this.orbitalPeriod = orbitalPeriod;
+            this.rotationalPeriod = rotationalPeriod;
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Planet : ");
+            base.Draw();
+        }
+
+        public double OrbitalRadius
+        {
+            get { return orbitalRadius; }
+            set { orbitalRadius = value; }
+        }
+
+        public double OrbitalPeriod
+        {
+            get { return orbitalPeriod; }
+            set { orbitalPeriod = value; }
+        }
+
+        public double RotationalPeriod
+        {
+            get { return rotationalPeriod; }
+            set { rotationalPeriod = value; }
+        }
+    }
+
+    public class Moon : Planet
+    {
+        public Moon(string name, double objectRadius, double rotationalPeriod, string objectColor,
+            double orbitalRadius, double orbitalPeriod)
+            : base(name, objectRadius, rotationalPeriod, objectColor, orbitalRadius, orbitalPeriod)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Moon : ");
+            base.Draw();
+        }
+    }
+
+    public class DwarfPlanet : Planet
+    {
+        public DwarfPlanet(string name, double objectRadius, double rotationalPeriod, string objectColor,
+            double orbitalRadius, double orbitalPeriod)
+            : base(name, objectRadius, rotationalPeriod, objectColor, orbitalRadius, orbitalPeriod)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.Write("Dwarf Planet : ");
+            base.Draw();
+        }
+    }
+}
