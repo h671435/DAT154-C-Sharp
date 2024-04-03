313f353f16b308e2399074aa9c609bbd8367c7a0
Storm <sangolts@gmail.com>
2024-04-03 12:10:59 +0200

    Ikke fungerende Coruses metoder og side

--- Oblig3/Oblig3/Models/Course.cs
+++ Oblig3/Oblig3/Models/Course.cs
@@ -1,7 +1,10 @@
-﻿namespace Oblig3;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oblig3;
 
 public class Course
 {
+    [Key]
     public string CourseCode { get; set; } = null!;
     public string CourseName { get; set; } = null!;
     public string Semester { get; set; } = null!;
---
+++ Oblig3/Oblig3/Models/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Oblig3;
+
+public class MyDbContext : DbContext
+{
+    public static DbSet<Course> Course { get; set; }
+    public DbSet<Grade> Grade { get; set; }
+    public DbSet<Student> Student { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "data source=dat154demo.database.windows.net;Initial Catalog=dat154;User ID=dat154_rw;Password=Svart_Katt;");
+    }
+    
+    public async Task<List<Student>> SearchStudents(string partialStudentName)
+    {
+        return await Student.Where(s => s.Studentname.Contains(partialStudentName)).ToListAsync();
+    }
+    
+    public IQueryable<Course> GetCourses()
+    {
+        return Course.AsQueryable();
+    }
+    
+}
